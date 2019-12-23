52d3b104ca650c4e7635a9ad381bf08abcc54d69
sera-hi <56292508+sera-hi@users.noreply.github.com>
2019-12-23 09:04:40 +0800

    Add files via upload

---
+++ if 语句.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+print("hello world")
+
+request_toppings = ['mushroom','red peppers','green peppers']
+for request_topping in request_toppings:
+    if request_topping == 'red peppers':
+        print('Sorry')
+    else:
+        print('adding '+request_topping+'into the cuisine')
+        
+alien_color = ['green','red','white']
+for color in alien_color:
+    if color == 'green':
+        print('you have got 5 points!')
+    else:
+        print('please try it again!')
+print('see you later!')
