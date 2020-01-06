c8d0da5241addeadbb06e53050744b8b09057eac
sera-hi <56292508+sera-hi@users.noreply.github.com>
2020-01-06 14:15:31 +0800

    Add files via upload
    list organization

---
+++ python_list_organization.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+print("hello world") #Good luck for today's learning!
+
+# List 组织列表
+cities = ['new york', 'paries', 'spain', 'sydney', 'Beijing']
+print(cities)
+print(sorted(cities))
+print(cities)
+cities.sort()
+print(cities)
+cities.sort(reverse = True)
+print(cities)
+cities.reverse()
+print(cities)
+
+
+
+
