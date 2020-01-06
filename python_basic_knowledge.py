9d26d30ebb5698a39eb820974dfdd8314956da98
sera-hi <56292508+sera-hi@users.noreply.github.com>
2020-01-06 09:32:41 +0800

    Add files via upload
    Beginning

---
+++ python_basic_knowledge.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print("hello world") #Good luck for today's learning!
+
+#Basic Knowledge
+
+# No need to worry about how to name your code: https://unbug.github.io/codelf/ 
+
+import this
+
