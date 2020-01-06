3f6902bcf12c817dd845f662d93864e57eef2c26
sera-hi <56292508+sera-hi@users.noreply.github.com>
2020-01-06 10:18:05 +0800

    Add files via upload
    list

---
+++ python_list.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+print("hello world") #Good luck for today's learning!
+
+# List
+names = ['wu yifan','cai xukun','yang mi','li xiaolu']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+
+print('hello, '+names[0].title()+', welcome to python world!')
+
